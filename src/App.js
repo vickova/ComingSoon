f47be4b10feb67b03b5c574c9418718a67735370
Victoria Olumide <olumideronke@gmail.com>
2022-09-07 19:28:43 -0700

    Respond

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ComingSoon from "./pages/ComingSoon";
-import { Security } from "./pages/Security";
 import { Route, Routes } from "react-router-dom";
 
 function App(){
@@ -9,7 +8,6 @@ function App(){
     <div className="App" style={{fontFamily:'sans-serif', backgroundColor:'#FFF'}}>
       <Routes>
         <Route path='/' element={<ComingSoon/>}/>
-        <Route path='/security' element={<Security/>}/>
       </Routes>
     </div>
   );
